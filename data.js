51e0fdc1d8fe8e6d8b4964a04229e6173ef65543
txygenova <cacagenopa@gmail.com>
2025-06-02 08:31:48 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,731 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dapur--musholah",
+      "name": "DAPUR & MUSHOLAH",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.6023167618337748,
+        "pitch": -0.012747886145842102,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7817691810748659,
+          "pitch": -0.0390234247500274,
+          "rotation": 0.7853981633974483,
+          "target": "10-taman-belakang"
+        },
+        {
+          "yaw": 2.2938145391794214,
+          "pitch": 0.021404786260065478,
+          "rotation": 5.497787143782138,
+          "target": "7-r-laundry"
+        },
+        {
+          "yaw": 2.4993951203275735,
+          "pitch": -0.0011278764877324932,
+          "rotation": 0.7853981633974483,
+          "target": "2-kamar-mandi"
+        },
+        {
+          "yaw": -2.238787831303963,
+          "pitch": -0.3225040033267863,
+          "rotation": 0.7853981633974483,
+          "target": "6-lantai-2"
+        },
+        {
+          "yaw": 3.083891613248942,
+          "pitch": 0.03530009382735422,
+          "rotation": 0,
+          "target": "9-ruang-tamu"
+        },
+        {
+          "yaw": -2.8609167344448636,
+          "pitch": 0.010569607121036384,
+          "rotation": 0,
+          "target": "3-kamar-tamu"
+        },
+        {
+          "yaw": 2.8391805365903595,
+          "pitch": -0.002784550023518406,
+          "rotation": 0,
+          "target": "1-exterior"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-exterior",
+      "name": "EXTERIOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5799816045211168,
+        "pitch": -0.00807706391882057,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9230649997205802,
+          "pitch": 0.0829947189867255,
+          "rotation": 4.71238898038469,
+          "target": "13-exterior-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kamar-mandi",
+      "name": "KAMAR MANDI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0639414687963455,
+          "pitch": -0.19396854272942932,
+          "rotation": 5.497787143782138,
+          "target": "0-dapur--musholah"
+        },
+        {
+          "yaw": 0.4570493076190427,
+          "pitch": -0.14617536779883267,
+          "rotation": 0.7853981633974483,
+          "target": "7-r-laundry"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kamar-tamu",
+      "name": "KAMAR TAMU",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5546979035845787,
+        "pitch": -0.005466457394579294,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.819713579851074,
+          "pitch": -0.09434124535615851,
+          "rotation": 5.497787143782138,
+          "target": "8-ruang-keluarga"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kamar-tidur-anak-1",
+      "name": "KAMAR TIDUR ANAK 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5621352401999715,
+        "pitch": -0.001978564316480913,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0235640084547963,
+          "pitch": -0.07769962788176699,
+          "rotation": 0.7853981633974483,
+          "target": "6-lantai-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-kamar-tidur-utama",
+      "name": "KAMAR TIDUR UTAMA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.580708106374333,
+        "pitch": 0.014684513882919248,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7970619336654252,
+          "pitch": -0.03390915292339258,
+          "rotation": 0,
+          "target": "6-lantai-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-lantai-2",
+      "name": "LANTAI 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5761662514408759,
+        "pitch": 0.016399372183766303,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9965363128901643,
+          "pitch": 0.48953899334703976,
+          "rotation": 16.493361431346422,
+          "target": "8-ruang-keluarga"
+        },
+        {
+          "yaw": 2.776703842045359,
+          "pitch": 0.011275784567086333,
+          "rotation": 5.497787143782138,
+          "target": "5-kamar-tidur-utama"
+        },
+        {
+          "yaw": 2.103158374494358,
+          "pitch": -0.16032182682739737,
+          "rotation": 4.71238898038469,
+          "target": "4-kamar-tidur-anak-1"
+        },
+        {
+          "yaw": 2.181463814908832,
+          "pitch": -0.008804033860212002,
+          "rotation": 0.7853981633974483,
+          "target": "11-kamar-mandi-lt-2"
+        },
+        {
+          "yaw": 3.035975442371152,
+          "pitch": -0.0036240325358249947,
+          "rotation": 0.7853981633974483,
+          "target": "12-kamar-tidur-anak-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-r-laundry",
+      "name": "R. LAUNDRY",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.053404564232334,
+        "pitch": 0.08386118901633566,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1106358269333185,
+          "pitch": -0.14461623601213347,
+          "rotation": 0,
+          "target": "8-ruang-keluarga"
+        },
+        {
+          "yaw": 1.4918997621570904,
+          "pitch": 0.0796180531775299,
+          "rotation": 11.780972450961727,
+          "target": "9-ruang-tamu"
+        },
+        {
+          "yaw": -2.7516492463055187,
+          "pitch": 0.030418439616187953,
+          "rotation": 5.497787143782138,
+          "target": "10-taman-belakang"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-ruang-keluarga",
+      "name": "RUANG KELUARGA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5520004733118746,
+        "pitch": 0.025510134508056836,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5671630778299432,
+          "pitch": -0.28547773279599653,
+          "rotation": 0.7853981633974483,
+          "target": "6-lantai-2"
+        },
+        {
+          "yaw": 0.7762494716204067,
+          "pitch": 0.011917529707833552,
+          "rotation": 4.71238898038469,
+          "target": "3-kamar-tamu"
+        },
+        {
+          "yaw": 2.9788775239968146,
+          "pitch": 0.16178770348091298,
+          "rotation": 0,
+          "target": "0-dapur--musholah"
+        },
+        {
+          "yaw": 0.03131698751157508,
+          "pitch": 0.08978256806218354,
+          "rotation": 0.7853981633974483,
+          "target": "9-ruang-tamu"
+        },
+        {
+          "yaw": -2.9377865224352586,
+          "pitch": -0.011795165297183274,
+          "rotation": 0.7853981633974483,
+          "target": "10-taman-belakang"
+        },
+        {
+          "yaw": -1.2127485325547696,
+          "pitch": 0.11064271953711113,
+          "rotation": 1.5707963267948966,
+          "target": "2-kamar-mandi"
+        },
+        {
+          "yaw": -1.6475714965191095,
+          "pitch": 0.05681411136834669,
+          "rotation": 0,
+          "target": "7-r-laundry"
+        },
+        {
+          "yaw": -0.35013561084337574,
+          "pitch": 0.03165759563400172,
+          "rotation": 0,
+          "target": "1-exterior"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-ruang-tamu",
+      "name": "RUANG TAMU",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5509548925376482,
+        "pitch": -0.005595204374822416,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5680317716687355,
+          "pitch": -0.1956655087980117,
+          "rotation": 6.283185307179586,
+          "target": "3-kamar-tamu"
+        },
+        {
+          "yaw": -3.0549651994496987,
+          "pitch": 0.1214402374180974,
+          "rotation": 0.7853981633974483,
+          "target": "2-kamar-mandi"
+        },
+        {
+          "yaw": -3.0613576688628257,
+          "pitch": -0.08404526165371706,
+          "rotation": 1.5707963267948966,
+          "target": "7-r-laundry"
+        },
+        {
+          "yaw": 2.6969818366596563,
+          "pitch": 0.08672012448858091,
+          "rotation": 4.71238898038469,
+          "target": "8-ruang-keluarga"
+        },
+        {
+          "yaw": 2.9620352356101414,
+          "pitch": 0.01154501269711794,
+          "rotation": 0,
+          "target": "0-dapur--musholah"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-taman-belakang",
+      "name": "TAMAN BELAKANG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.2889285616591888,
+        "pitch": 0.016399372183755645,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.45938107977857534,
+          "pitch": -0.017406905091608138,
+          "rotation": 4.71238898038469,
+          "target": "0-dapur--musholah"
+        },
+        {
+          "yaw": -1.2994092475156584,
+          "pitch": 0.010855735146874324,
+          "rotation": 0.7853981633974483,
+          "target": "7-r-laundry"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-kamar-mandi-lt-2",
+      "name": "KAMAR MANDI LT. 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7402612624346006,
+          "pitch": -0.3263101057354003,
+          "rotation": 5.497787143782138,
+          "target": "6-lantai-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-kamar-tidur-anak-2",
+      "name": "KAMAR TIDUR ANAK 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5560275651815365,
+        "pitch": 0.02550933236367392,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7918110435483499,
+          "pitch": -0.07893110882535304,
+          "rotation": 10.995574287564278,
+          "target": "6-lantai-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-exterior-2",
+      "name": "exterior 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.432769406709264,
+        "pitch": -0.028516963994476896,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8270805739823555,
+          "pitch": 0.050528522593403125,
+          "rotation": 0,
+          "target": "9-ruang-tamu"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-exterior",
+      "name": "EXTERIOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5741780286290084,
+        "pitch": -0.036545829751570835,
+        "fov": 1.4413225997054397
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9306502487856285,
+          "pitch": 0.09884030551442535,
+          "rotation": 5.497787143782138,
+          "target": "13-exterior-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
